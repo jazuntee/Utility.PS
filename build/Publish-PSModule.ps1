7ad7cabef1e922c756239d48d7c6154aaff67102
Jason Thompson <jasoth@microsoft.com>
2020-07-31 12:13:26 -0400

    Update version

--- build/Publish-PSModule.ps1
+++ build/Publish-PSModule.ps1
@@ -2,7 +2,7 @@ param
 (
     # Path to Module
     [Parameter(Mandatory = $false)]
-    [string] $ModulePath = ".\release\Utility.PS\1.0.5",
+    [string] $ModulePath = ".\release\Utility.PS\1.0.6",
     # API Key for PowerShell Gallery
     [Parameter(Mandatory = $true)]
     [string] $NuGetApiKey
